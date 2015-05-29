4a18c8757b42c6e03588778e1a2b2fc1b3d50523
8rrico <8rrico@gmail.com>
2015-05-29 13:25:53 -0400

    Improve

--- DeathBag/src/me/bukkit/deathbag/Inkzzz/Main.java
+++ DeathBag/src/me/bukkit/deathbag/Inkzzz/Main.java
@@ -5,11 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new PlayerDeath(), this);
+		this.getServer().getPluginManager().registerEvents(new PlayerDeath(), this);
 	}
-	
-	public void onDisable() {
-		
-	}
-	 
+
 }
