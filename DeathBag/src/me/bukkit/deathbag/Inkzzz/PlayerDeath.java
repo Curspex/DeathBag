71f9f03fc18c1a148d294999bc322f5d2501124b
8rrico <8rrico@gmail.com>
2015-05-29 13:26:38 -0400

    Redo this, update with Java 8 methods. Not gonna add all the saftey checks that should be in place, leaving that to the author.

--- DeathBag/src/me/bukkit/deathbag/Inkzzz/PlayerDeath.java
+++ DeathBag/src/me/bukkit/deathbag/Inkzzz/PlayerDeath.java
@@ -2,27 +2,31 @@ package me.bukkit.deathbag.Inkzzz;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Chest;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 
 public class PlayerDeath implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGH)
-	public void onPlayerDeath(PlayerDeathEvent e) {
-		Player p = (Player) e.getEntity();
-		Location ploc = p.getLocation();
-		ploc.getWorld().getBlockAt(ploc).setType(Material.CHEST);
-		Chest chest = (Chest) ploc.getBlock().getState();
-		for (ItemStack is : e.getDrops()) {
-			chest.getInventory().addItem(is);
-		}
-		chest.update();
-		e.getDrops().clear();
+	public void onPlayerDeath(final PlayerDeathEvent event) {
+		if (event.getKeepInventory() == true || !event.getEntityType().equals(EntityType.PLAYER) || event.getDrops().size() < 1) return;
+		final Location location = event.getEntity().getLocation();
+		location.getBlock().setType(Material.CHEST);
+		final Inventory inv = ((InventoryHolder) location.getBlock().getState()).getInventory();
+		
+		event.getDrops().forEach(drop -> inv.addItem(drop));
+		
+		//for (final ItemStack is : e.getDrops())
+		//{
+		//	chest.getInventory().addItem(new ItemStack[] { is });
+		//}
+
+		event.getDrops().clear();
 	}
 
 }
